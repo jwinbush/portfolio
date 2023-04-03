aa2f4230ff70976c4956ee4728d53378bf8f6854
J.M Winbush <jawonwinbush@gmail.com>
2023-04-03 02:20:00 -0400

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -153,7 +153,26 @@ export default function Home() {
               >
                 Repository
               </a>
-             
+              {/* <a
+                href="/"
+                rel="noopener noreferrer"
+                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center rounded-md text-black bg-gradient-to-br from-cyan-400 via-emerald-300 to-cyan-400 hover:bg-gradient-to-br shadow-lg shadow-cyan-400/50 animate-text"
+              >
+                Live Demo
+                <svg
+                  aria-hidden="true"
+                  className="w-4 h-4 ml-2 -mr-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </a> */}
               <div className="px-6 py-4">
                 <span className="inline-block bg-gray-100 rounded-md px-3 py-1 text-sm font-semibold text-gray-600 mr-2">
                   oop-php
