1d035de2736315508757d929259a456a45e61048
J.M Winbush <jawonwinbush@gmail.com>
2023-04-29 13:19:57 -0400

    Update Social.js

--- src/components/social-icons/Social.js
+++ src/components/social-icons/Social.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Social() {
   return (
     <div class="fixed bottom-0 left-0 flex flex-col space-y-1 text-white">
-      <Link to="/contact" class="p-2 rounded-full" target="_blank">
+      <Link to="/contact" class="p-2 rounded-full">
         <FaMailBulk width={20} />
       </Link>
       <Link
